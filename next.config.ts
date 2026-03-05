ce9fde83b95398d6e3b70c88847c6a12ed4d3770
optmus825454 <ikinciyenikitap54@gmail.com>
2026-03-05 20:57:07 +0300

    pp

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://cdn.jsdelivr.net",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: blob: https:",
               "font-src 'self' data:",
